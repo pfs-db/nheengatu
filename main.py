0d41cf9d74b95f670b640615ff4a06d6cfcf6d67
PabloFreitas-lib <pablofreitas.fn@gmail.com>
2024-05-26 19:37:11 +0200

    Command line interaction - Argparse - Added a simple argument into the main.py - Configured the debug file (VS_Code) to interact with the code and understand the work flow

--- main.py
+++ main.py
@@ -5,17 +5,32 @@ import os, sys, datetime
 from src.BuildDictionary import loadLexicon
 from src.common_paths import module_path
 import src.common_paths as cp
+import argparse
 
 
-def main(infile):
+def handle_args():
+    """tmp function to show how the command lines interaction could be used"""
+    parser = argparse.ArgumentParser(
+        prog="nheengatu",
+        description="Automatically POS-tagged by Nheengatagger",
+        epilog="Text at the bottom of help",
+    )
+    parser.add_argument("infile")
+    return parser
+
+
+def main():
+    parser = handle_args()
+    args = parser.parse_args()
+
     DICTIONARY = nh.buildDictionary(cp.LEXICON_PATH)
     WE = nh.extractMWEs(DICTIONARY)
     print(cp.MESSAGE)
     lines = []
-    with open(infile) as f:
+    with open(args.infile) as f:
         lines = f.readlines()
     nh.tagText(lines)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])  # TODO use argparse module
+    main()
